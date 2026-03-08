f1f3e5481d0365a72955f4ce291303115e31e359
antjekunert4-png <antjekunert4@gmail.com>
2026-03-08 18:25:21 +0100

    added some of the weather information

--- src/index.js
+++ src/index.js
@@ -2,14 +2,22 @@ function updateWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.data.city;
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
 
   temperatureElement.innerHTML = Math.round(temperature);
+  cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  timeElement.innerHTML = `${date.getDay()} ${date.getHours()}:${date.getMinutes()};
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 }
 function searchCity(city) {
   let apiKey = "a75f8bb18344f8at30c2o163eff475bb";
   let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-
   axios.get(apiURL).then(updateWeather);
 }
 function handleSearchSubmit(event) {
