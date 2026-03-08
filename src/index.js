32db94098e465e5702ee7573827b7cb602a773c0
antjekunert4-png <antjekunert4@gmail.com>
2026-03-08 15:10:01 +0100

    added search functionality using the API

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+function searchCity(city) {
+  let apiKey = "a75f8bb18344f8at30c2o163eff475bb";
+  let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiURL).then(updateWeather);
+}
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
