0847af87f997ec5160d3ad9b3ef34d56ed177b74
antjekunert4-png <antjekunert4@gmail.com>
2026-03-08 17:17:30 +0100

    fixed a mistake

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 function updateWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
 
   temperatureElement.innerHTML = Math.round(temperature);
 }
@@ -13,8 +15,6 @@ function searchCity(city) {
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
 
   searchCity(searchInput.value);
 }
